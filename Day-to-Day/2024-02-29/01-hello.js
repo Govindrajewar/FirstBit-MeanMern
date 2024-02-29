a8ba75ffee2b97560a4b89bebeb4024af3dad635
Govind Rajewar <72288656+Govindrajewar@users.noreply.github.com>
2024-02-29 17:40:31 +0530

    Installation of Node & Typ

---
+++ Day-to-Day/2024-02-29/01-hello.js
@@ -0,0 +1,11 @@
+/**
+ * Commands to execute .ts files
+ * 1. cd .\Day-to-Day\2024-02-29
+ * 2. tsc .\01-hello.ts
+ *
+ */
+console.log("Hello World!");
+var a1 = 10;
+var b1 = 20;
+var c1 = 30;
+console.log(a1, b1, c1);
---
+++ Day-to-Day/2024-02-29/01-hello.ts
@@ -0,0 +1,13 @@
+/**
+ * Commands to execute .ts files
+ * 1. cd .\Day-to-Day\2024-02-29
+ * 2. tsc .\01-hello.ts
+ */
+
+console.log("Hello World!");
+
+var a1 =10;
+let b1 = 20;
+const c1 = 30;
+
+console.log(a1, b1, c1);
